b970391155e894a852aa32ea932ebc460e3801fa
Daniel de Andrade Mendes <141786255+BluDani@users.noreply.github.com>
2023-09-05 11:22:47 -0300

    Create ex08_alterar.php

---
+++ ex08_alterar.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nome = "";
+    $matricula = "";
+    $email = "";
+
+    $arqAluno = fopen("alunos.txt", "r") or die("erro ao criar arquivo");
+    $arqAux = fopen("alunos.txt", "a") or die("erro ao criar arquivo");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ALterar Dado</title>
+</head>
+<body>
+    
+</body>
+</html>
