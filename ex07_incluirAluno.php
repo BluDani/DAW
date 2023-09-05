2ef906f3f1cc151d6345ddc35d3bdebefa61166f
Daniel de Andrade Mendes <141786255+BluDani@users.noreply.github.com>
2023-09-04 21:32:56 -0300

    Update ex07_incluirAluno.php

--- ex07_incluirAluno.php
+++ ex07_incluirAluno.php
@@ -21,11 +21,10 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Incluir Aluno</title>
 </head>
 <body>
         <h1>Novo Aluno</h1>
