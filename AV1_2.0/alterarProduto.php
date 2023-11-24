969eda36ff369c397472a1471f6537795d738dce
Daniel de Andrade Mendes <141786255+BluDani@users.noreply.github.com>
2023-11-24 08:55:34 -0300

    Update alterarProduto.php

--- AV1_2.0/alterarProduto.php
+++ AV1_2.0/alterarProduto.php
@@ -1,4 +1,5 @@
 <?php
+
     $id = $_POST["id"];
     $nome = $_POST["nome"];
     $valor = $_POST["valor"];
@@ -10,13 +11,12 @@
     $conn = new mysqli($servidor,$username,$senha,$database);
 
     if($conn->connect_error){
-
        die("Conexao falhou, avise o administrador do sistema");
     }
 
-    $comandoSQL = "UPDATE `produtos` SET nome=$nome,valor=$valor WHERE id=$id";
+    $comandoSQL = "UPDATE `produtos` SET nome = '$nome', valor = '$valor' WHERE id = $id";
     $resultado = $conn->query($comandoSQL);
 
-    $retorno=json_encode($resultado);
+    $retorno = json_encode($resultado);
     echo $retorno;
 ?>
