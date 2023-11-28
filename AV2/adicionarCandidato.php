0434a00a18f73e88e209354e74da293cfbf84588
Daniel de Andrade Mendes <141786255+BluDani@users.noreply.github.com>
2023-11-28 12:06:43 -0300

    Add files via upload

---
+++ AV2/adicionarCandidato.php
@@ -0,0 +1,30 @@
+<?php
+
+    $nome = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $identidade = $_POST["identidade"];
+    $email = $_POST["email"];
+    $cargo = $_POST["cargo"];
+    $sala = $_POST["sala"];
+
+    $servidor = "localhost";
+    $username = "root";
+    $senha = "";
+    $database = "pessoal";
+
+    $conn = new mysqli($servidor,$username,$senha,$database);
+
+    if($conn->connect_error){
+
+        die("Conexao falhou, avise o administrador do sistema");
+    }
+
+    $comandoSQL = "INSERT INTO `candidatos` (nome,cpf,identidade,email,cargo,sala) values ('$nome', '$cpf', 
+    '$identidade', '$email', '$cargo', $sala)";
+    
+    $resultado = $conn->query($comandoSQL);
+
+    $retorno=json_encode($resultado);
+    echo $retorno;
+    
+?>
